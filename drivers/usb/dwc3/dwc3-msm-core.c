d16e801e803b7926e164a46d776ac57015e9a0b0
Rajashekar kuruva <quic_kuruva@quicinc.com>
2025-07-24 16:29:24 +0530

    usb: dwc3: msm: Fix regulator error handling in vbus_regulator_get
    Fixes the error handling returned by the devm_regulator_get_optional()
    
    Previously, the code returned success for a potentially
    invalid pointer after checking for -EPROBE_DEFER without
    assigning the pointer to null.
    
    The updated logic ensures proper error checking and assigns the
    pointer to NULL before any dereferencing or further error handling.
    
    Change-Id: Ifbc918bcdfcc7c5e4f6e14a9ee8c624ce569acf2
    Signed-off-by: Rajashekar kuruva <quic_kuruva@quicinc.com>

--- drivers/usb/dwc3/dwc3-msm-core.c
+++ drivers/usb/dwc3/dwc3-msm-core.c
@@ -6278,7 +6278,7 @@ static int dwc3_msm_check_extcon_prop(struct platform_device *pdev)
 	return ret;
 }
 
-static void vbus_regulator_get(struct dwc3_msm *mdwc)
+static int vbus_regulator_get(struct dwc3_msm *mdwc)
 {
 	/*
 	 * The vbus_reg pointer could have multiple values
@@ -6288,13 +6288,16 @@ static void vbus_regulator_get(struct dwc3_msm *mdwc)
 	 */
 	mdwc->vbus_reg = devm_regulator_get_optional(mdwc->dev,
 						"vbus_dwc3");
-	if (IS_ERR(mdwc->vbus_reg)) {
-		dev_err(mdwc->dev, "Unable to get vbus regulator err: %ld\n",
-							PTR_ERR(mdwc->vbus_reg));
-		mdwc->vbus_reg = NULL;
-		return;
-	}
+	if (!IS_ERR(mdwc->vbus_reg))
+		return 0;
 
+	/* regulators may not be ready, so retry again later */
+	if (PTR_ERR(mdwc->vbus_reg) == -EPROBE_DEFER)
+		return -EPROBE_DEFER;
+
+	mdwc->vbus_reg = NULL;
+
+	return 0;
 }
 
 static int dwc3_msm_probe(struct platform_device *pdev)
@@ -6484,7 +6487,9 @@ static int dwc3_msm_probe(struct platform_device *pdev)
 	pm_runtime_use_autosuspend(mdwc->dev);
 	device_init_wakeup(mdwc->dev, 1);
 
-	vbus_regulator_get(mdwc);
+	ret = vbus_regulator_get(mdwc);
+	if (ret < 0)
+		goto err;
 
 	if (of_property_read_bool(node, "qcom,disable-dev-mode-pm"))
 		pm_runtime_get_noresume(mdwc->dev);
