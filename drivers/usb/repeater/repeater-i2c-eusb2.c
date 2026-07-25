nekoshirro/platform_kernel_xiaomi_marble

usb: phy: Enable auto-resume WA if repeater doesn't support
Curretly auto-resume feature is not available on all third party
repeaters which are used. But there are certain revisions of
repeaters which implemented auto-resume feature. Since, the WA
was to keep the ref_clk enabled as part of host mode bus suspend
so that the ref_clk for the PHY never gets turned off in active
usecases, it imposes power penalty in suspend usecases.

This change enables auto-resume WA for the revision of repeaters
that doesn't support the feature.

Change-Id: Ie0556bce34bbc2d321ed821c6beb7ba4a43f3ffd
Signed-off-by: Pratham Pratap <quic_ppratap@quicinc.com>

drivers/usb/repeater/repeater-i2c-eusb2.c
@@ -247,6 +247,9 @@ static int eusb2_repeater_init(struct usb_repeater *ur)
 	switch (chip->repeater_type) {
 	case TI_REPEATER:
 		eusb2_i2c_read_reg(er, REV_ID, &reg_val);
+		/* If the repeater revision is B1 disable auto-resume WA */
+		if (reg_val == 0x03)
+			ur->flags |= UR_AUTO_RESUME_SUPPORTED;
 		break;
 	case NXP_REPEATER:
 		eusb2_i2c_read_reg(er, REVISION_ID, &reg_val);
@@ -255,7 +258,7 @@ static int eusb2_repeater_init(struct usb_repeater *ur)
 		dev_err(er->ur.dev, "Invalid repeater\n");
 	}
 
-	dev_info(er->ur.dev, "eUSB2 repeater version = 0x%x\n", reg_val);
+	dev_info(er->ur.dev, "eUSB2 repeater version = 0x%x ur->flags:0x%x\n", reg_val, ur->flags);
 
 	/* override init sequence using devicetree based values */
 	if (er->param_override_seq_cnt)

elginsk8r/android_kernel_qcom_sm8450

drivers/usb/repeater/repeater-i2c-eusb2.c
@@ -251,6 +251,9 @@ static int eusb2_repeater_init(struct usb_repeater *ur)
 	switch (chip->repeater_type) {
 	case TI_REPEATER:
 		eusb2_i2c_read_reg(er, REV_ID, &reg_val);
+		/* If the repeater revision is B1 disable auto-resume WA */
+		if (reg_val == 0x03)
+			ur->flags |= UR_AUTO_RESUME_SUPPORTED;
 		break;
 	case NXP_REPEATER:
 		eusb2_i2c_read_reg(er, REVISION_ID, &reg_val);
@@ -260,7 +263,7 @@ static int eusb2_repeater_init(struct usb_repeater *ur)
 		return -EINVAL;
 	}
 
-	dev_dbg(er->ur.dev, "eUSB2 repeater version = 0x%x\n", reg_val);
+	dev_dbg(er->ur.dev, "eUSB2 repeater version = 0x%x ur->flags:0x%x\n", reg_val, ur->flags);
 
 	/* override init sequence using devicetree based values */
 	if (er->param_override_seq_cnt)